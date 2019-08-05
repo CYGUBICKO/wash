46f2018565eff37e60c223441708b605f2ce4d43
cygubicko <cygu@aims.ac.za>
2019-08-05 19:32:55 -0400

    Autocommit (wash)

--- simulations_plots_temp.R
+++ simulations_plots_temp.R
@@ -18,7 +18,9 @@ load("rstanarmModelbin.rda")
 rstanmodel <- rstanmodel_list[[1]]
 
 plot_df <- plot(rstanmodel)[["data"]]
-summary_plot <- (plot_df
+
+# Population level estimate plot
+population_est_plot <- (plot_df
 	%>% filter(grepl("^y[1-3]", parameter))
 	%>% ggplot(aes(x = reorder(parameter, m), y = m))
 		+ geom_hline(yintercept = 0, size = 1/2, color = "firebrick4", alpha = 1/10)
@@ -32,82 +34,135 @@ summary_plot <- (plot_df
 			, x = parameter
 			, xend = parameter
 		) 
-			, size = 0.5, color = "azure3"
+			, size = 0.8, color = "lightblue"
 		)
 		+ geom_pointrange(aes(ymin = l
 			, ymax = h
 		)
-			, size = 0.7, color = ""
+			, size = 1, color = "deepskyblue4"
 		)
-		+ geom_point(color = "azure3", size = 3)
+		+ geom_point(color = "lightblue", size = 3.5)
 		+ coord_flip()
 		+ labs(x = NULL, y = NULL)
 		+ theme_bw()
 )
-summary_plot
+population_est_plot
 
-plot(rstanmodel, regex_pars = "^y[1-3]\\|\\(Intercept\\)|wealthindex")
-
-
-
-
-quit()
-summary_plot <- (plot_df
-	%>% filter(grepl("^y[1-3]", parameter))
+# Sigma
+sigma_est_plot <- (plot_df
+	%>% filter(grepl("^Sigma", parameter))
+	%>% mutate(ny = ifelse(lengths(regmatches(parameter, gregexpr("y1", parameter))) == 2
+			, 2 
+			, ifelse(lengths(regmatches(parameter, gregexpr("y2", parameter))) == 2
+				, 2
+				, ifelse(lengths(regmatches(parameter, gregexpr("y3", parameter))) == 2
+					, 2
+					, lengths(regmatches(parameter, gregexpr("y1|y2|y2", parameter)))
+				) 
+			)
+		)
+	)
+	%>% filter(ny == 2)
 	%>% ggplot(aes(x = reorder(parameter, m), y = m))
-#		+ geom_point(color = "deepskyblue", size = 3, alpha = 1/10)
 		+ geom_hline(yintercept = 0, size = 1/2, color = "firebrick4", alpha = 1/10)
+		+ geom_pointrange(aes(ymin = l
+			, ymax = h
+		)
+			, size = 2/5, color = "deepskyblue4"
+		)
 		+ geom_segment(aes(y = ll
 			, yend = hh
 			, x = parameter
 			, xend = parameter
 		) 
-			, size = 2/5, color = "deepskyblue"
+			, size = 0.8, color = "lightblue"
 		)
 		+ geom_pointrange(aes(ymin = l
 			, ymax = h
 		)
-			, size = 1, color = "deepskyblue4", alpha = 1/10
+			, size = 1, color = "deepskyblue4"
 		)
+		+ geom_point(color = "lightblue", size = 3.5)
 		+ coord_flip()
 		+ labs(x = NULL, y = NULL)
 		+ theme_bw()
 )
-summary_plot
-summary_plot <- (rstanmodel
-	%>% summary()
-	%>% data.frame()
-	%>% rownames_to_column("terms")
-	%>% filter(!terms %in% c("log-posterior", grep("_PPD", terms, value = TRUE)))
-	%>% select(c("terms", "mean", "sd", "X2.5.", "X50.", "X97.5.", "n_eff"))
-	%>% setnames(names(.), c("terms", "mean", "sd", "lb", "median", "lu", "n_eff"))
-	%>% ggplot(aes(x = reorder(terms, mean), y = mean))
-		+ geom_hline(yintercept = 0, size = 1/2, color = "firebrick4", alpha = 1/10)
-		+ geom_pointrange(aes(ymin = lb, ymax = lu), size = 2/5, shape = 20, color = "firebrick4")
-		+ geom_segment(aes(y = lb
-			, yend = lu
-			, x = terms
-			, xend = terms
-		) 
-			, size = 3, color = "firebrick4", alpha = 1/10
+sigma_est_plot
+
+# Group level estimates - Year
+year_est_plot <- (plot_df
+	%>% filter(grepl("^b\\[", parameter) & grepl("years", parameter))
+	%>% mutate(variables = ifelse(grepl("y1", parameter), "y1"
+			, ifelse(grepl("y2", parameter), "y2"
+				, "y3"
+			)
 		)
+		, parameter = gsub("y[1-3]", "", parameter)
+	)
+	%>% ggplot(aes(x = reorder(parameter, m), y = m))
+		+ geom_hline(yintercept = 0, size = 1/2, color = "firebrick4", alpha = 1/10)
+		+ geom_pointrange(aes(ymin = l
+			, ymax = h
+		)
+			, size = 2/5, color = "deepskyblue4"
+		)
+		+ geom_segment(aes(y = ll
+			, yend = hh
+			, x = parameter
+			, xend = parameter
+		) 
+			, size = 0.8, color = "lightblue"
+		)
+		+ geom_pointrange(aes(ymin = l
+			, ymax = h
+		)
+			, size = 1, color = "deepskyblue4"
+		)
+		+ geom_point(color = "lightblue", size = 3.5)
+		+ facet_grid(.~ variables, scales = "free")
 		+ coord_flip()
+		+ labs(x = NULL, y = NULL)
+		+ theme_bw()
 )
-summary_plot
-
-
-posterior_df <- as.matrix(rstanmodel)
-
-summary(rstanmodel)
+year_est_plot
 
 quit()
-posterior_estimates <- (t(apply(posterior_df, 2, quantile, probs = c(0.1, 0.5, 0.9)))
-	%>% data.frame()
-	%>% rownames_to_column("terms")
-	%>% mutate(Variable = ifelse(grepl("y1|", terms), "y1"
-   	, ifelse(grepl("y2|", terms), "y2", "y3")
-   	)
+patterns <- c("^b\\[y1", "^b\\[y2", "^b\\[y3")
+
+year_est_plots <- list()
+for (i in 1:length(patterns)){
+	year_est_plot <- (plot_df
+		%>% filter(grepl(patterns[i], parameter) & grepl("years", parameter))
+		%>% ggplot(aes(x = reorder(parameter, m), y = m))
+			+ geom_hline(yintercept = 0, size = 1/2, color = "firebrick4", alpha = 1/10)
+			+ geom_pointrange(aes(ymin = l
+				, ymax = h
+			)
+				, size = 2/5, color = "deepskyblue4"
+			)
+			+ geom_segment(aes(y = ll
+				, yend = hh
+				, x = parameter
+				, xend = parameter
+			) 
+				, size = 0.8, color = "lightblue"
+			)
+			+ geom_pointrange(aes(ymin = l
+				, ymax = h
+			)
+				, size = 1, color = "deepskyblue4"
+			)
+			+ geom_point(color = "lightblue", size = 3.5)
+			+ coord_flip()
+			+ labs(x = NULL, y = NULL)
+			+ theme_bw()
 	)
+	year_est_plots[[i]] <- year_est_plot
+}
+
+bayesplot_grid(year_est_plots[[1]]
+	, year_est_plots[[2]]
+	, year_est_plots[[3]]
+	, grid_args = list(ncol = 1)
 )
 
-posterior_estimates
