3376dc485448b6b231e6a710e3a2a1cb09f403d8
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-02-24 17:13:45 -0500

    Meeting with Steve

--- missing.R
+++ missing.R
@@ -5,6 +5,11 @@
 #### ---- By: Steve and Jonathan ----
 #### ---- Date: 2019 Dec 24 (Tue) ----
 
+## 1.374/3.184 ## .432 ## Proportion of points with no previous
+## .3741/2.184 ## .171 ## Proportion of followups unusable
+## .3741/3.184 ## .118 ## Proportion of data unusable
+## git_push/washdataAnalysis_report.html
+
 library(dplyr)
 library(tidyr)
 library(data.table)
@@ -23,4 +28,16 @@ missing <- (wash_consec_df
 	)
 )
 
-summary(missing)
+mean_h <- (missing
+	%>% select(-hhid)
+	%>% summarise_all(funs(mean))
+)
+
+print(mean_h
+	%>% transmute(
+		no_prev = all_miss/n
+		, follow_unused = real_miss/(n-1) ## Proportion of things that aren't first that we can't use
+		, obs_unused = real_miss/n ## Proportion That we don't use at all
+	)
+)
+
